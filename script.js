69b5caa1a23842d38ccbaca2e94fa2beb3c7afad
judy-chelangat <judysigilai75@gmail.com>
2023-07-05 22:00:59 +0300

    added an event listener on the form and retrieved the search item

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-//step 1 I  want to fetch a list of all the available jobs and display it on the dom  
-//so I will have to implement a function that fetches the data and display it on the dom when the page loads 
+// //step 1 I  want to fetch a list of all the available jobs and display it on the dom  
+// //so I will have to implement a function that fetches the data and display it on the dom when the page loads 
 const initialize = ()=>{  //function to hold the entire code 
 // fetching the job details from server
 function jobDetails(){
     fetch("http://localhost:3000/jobs")
     .then(resp => resp.json())
     .then(data =>{
-        data.forEach(list =>{
-        fetchJobs(list) // passing in a function to the data returned
+                // console.log(data)
+        data.forEach(job=>{
+        displayJobs(job) // passing in a function to the data returned
         });
-      
-        console.log(data)
-    })
 
-    function fetchJobs(list){
+        //event listener on the form
+        const jobSearchForm = document.getElementById("jobSearchForm");
+        jobSearchForm.addEventListener("submit", function (e) {
+          e.preventDefault();
+
+          // Retrieve the search term entered by the user
+          const roleInput = document.getElementById("roleInput");
+          const searchTerm = roleInput.value.toLowerCase().trim();
+
+          // Filter the job list based on the search term
+          const filteredJobs = jobList.filter(job => {
+            const position = job.position.toLowerCase();
+            return position.includes(searchTerm);
+          });
+
+          // Clear the existing job list on the DOM
+          clearJobList();
+
+          // Display the filtered job results on the DOM
+          filteredJobs.forEach(job =>{
+            displayJobs(job)
+          });
+        });
+      })
+         .catch(error =>{
+            console.log(error)
+         })
+
+        function displayJobs(list){
         const mainContainer =document.getElementById("jobList")
        const details = document.createElement("div")
        details.className = "job-details"; // add class name for styling
