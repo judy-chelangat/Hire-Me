321056c734bf2a9c6f38ae83e70d5cb1d3222c74
judy-chelangat <judysigilai75@gmail.com>
2023-07-05 13:21:30 +0300

    added a function that fetches the details of the job and display it when the browser loads

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
+//step 1 I  want to fetch a list of all the available jobs and display it on the dom  
+//so I will have to implement a function that fetches the data and display it on the dom when the page loads 
 const initialize = ()=>{  //function to hold the entire code 
-// fetching the job details from an API
-function fetchJobs(){
+// fetching the job details from server
+function jobDetails(){
     fetch("http://localhost:3000/jobs")
     .then(resp => resp.json())
     .then(data =>{
+        data.forEach(list =>{
+        fetchJobs(list) // passing in a function to the data returned
+        });
+      
         console.log(data)
     })
+
+    function fetchJobs(list){
+        const mainContainer =document.getElementById("jobList")
+       const details = document.createElement("div")
+       details.className = "job-details"; // add class name for styling
+// dom manipulation to show the job details
+          details.innerHTML=`
+          <div class="left-side">
+                <div class="img src ="${list.logo}"</div
+                <div>
+                    <h6>${list.position}</h6>
+                    <p>${list.postedAt}</p>
+                    <p>${list.contract}</p>
+                    <p>${list.location}</p>
+                </div>
+          </div>
+          
+          <div class="right-side "
+             <p>${list.languages}</p>
+          </div>
+          
+          
+          `
+          mainContainer.appendChild(details)
+    }
 }
-fetchJobs()
+jobDetails()
 }
 
 
