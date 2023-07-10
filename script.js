0c2608d58475fad43fa09758c1c90e9611992c6a
judy-chelangat <judysigilai75@gmail.com>
2023-07-10 20:47:03 +0300

    formatted the entire code with prettier

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-// //step 1 I  want to fetch a list of all the available jobs and display it on the dom  
-// //so I will have to implement a function that fetches the data and display it on the dom when the page loads 
-const initialize = ()=>{  //function to hold the entire code 
-// fetching the job details from server
-function jobDetails(){
+// //step 1 I  want to fetch a list of all the available jobs and display it on the dom
+// //so I will have to implement a function that fetches the data and display it on the dom when the page loads
+const initialize = () => {
+  //function to hold the entire code
+  // fetching the job details from server
+  function jobDetails() {
     fetch("https://hireme-9kv4.onrender.com/jobs")
-    .then(resp => resp.json())
-    .then(data =>{
-                // console.log(data)
-        data.forEach(job=>{
-        displayJobs(job) // passing in a function to the data returned
+      .then((resp) => resp.json())
+      .then((data) => {
+        // console.log(data)
+        data.forEach((job) => {
+          displayJobs(job); // passing in a function to the data returned
         });
 
         //event listener on the form
         const jobSearchForm = document.getElementById("jobSearchForm");
-        jobSearchForm.addEventListener("submit", jobHandle)
-        //function to handle the job searched for 
-        function jobHandle(e ) {
+        jobSearchForm.addEventListener("submit", jobHandle);
+        //function to handle the job searched for
+        function jobHandle(e) {
           e.preventDefault();
 
           // Retrieve the search term entered by the user
@@ -23,12 +24,12 @@ function jobDetails(){
           const searchTerm = roleInput.value.toLowerCase().trim();
 
           //if statement to check whether the searchterm is empty
-          if(searchTerm === " "){
-            displayJobs(job)
+          if (searchTerm === " ") {
+            displayJobs(job);
           }
-          
+
           // Filter the job list based on the search term
-          const filteredJobs = data.filter(job => {
+          const filteredJobs = data.filter((job) => {
             const position = job.position.toLowerCase();
             return position.includes(searchTerm);
           });
@@ -37,21 +38,21 @@ function jobDetails(){
           clearJobList();
 
           // Display the filtered job results on the DOM
-          filteredJobs.forEach(job =>{
-            displayJobs(job)
+          filteredJobs.forEach((job) => {
+            displayJobs(job);
           });
-        };
-      })
-        //  .catch(error =>{
-        //     console.log("error")
-        //  })
+        }
+      });
+    //  .catch(error =>{
+    //     console.log("error")
+    //  })
 
-        function displayJobs(list){
-        const mainContainer =document.getElementById("jobList")
-       const details = document.createElement("div")
-       details.className = "job-details"; // add class name for styling
-        // dom manipulation to show the job details
-          details.innerHTML=`
+    function displayJobs(list) {
+      const mainContainer = document.getElementById("jobList");
+      const details = document.createElement("div");
+      details.className = "job-details"; // add class name for styling
+      // dom manipulation to show the job details
+      details.innerHTML = `
           <div class="left-side">
                 <div class="img"><img src="${list.logo}"></div>
 
@@ -66,114 +67,99 @@ function jobDetails(){
             <a href="${list.link}">Apply Now</a>
             <button class="remove-job">X</button> <!-- Add the X button -->
         </div>
-          `
-          // adding an event listener on the x button
-          const removeJob= details.querySelector(".remove-job");
-          removeJob.addEventListener("click",function(){
-            details.remove()
-            // deleteJobDetails(list.id)
-         })
-          mainContainer.appendChild(details)
+          `;
+      // adding an event listener on the x button
+      const removeJob = details.querySelector(".remove-job");
+      removeJob.addEventListener("click", function () {
+        details.remove();
+        // deleteJobDetails(list.id)
+      });
+      mainContainer.appendChild(details);
     }
-    
-}
- // Function to clear the existing job list on the DOM
- function clearJobList() {
+  }
+  // Function to clear the existing job list on the DOM
+  function clearJobList() {
     const mainContainer = document.getElementById("jobList");
     mainContainer.innerHTML = "";
   }
-jobDetails()
-      // const jobButton = document.getElementById("jobButton");
-      // const jobPopupForm = document.getElementById("jobPopupForm");
-      // const overlay = document.getElementById("overlay");
+  jobDetails();
+  // const jobButton = document.getElementById("jobButton");
+  // const jobPopupForm = document.getElementById("jobPopupForm");
+  // const overlay = document.getElementById("overlay");
 
-      // jobButton.addEventListener("click", function() {
-      //     jobPopupForm.style.display = "block";
-      //     overlay.style.display = "block";
-      // });
+  // jobButton.addEventListener("click", function() {
+  //     jobPopupForm.style.display = "block";
+  //     overlay.style.display = "block";
+  // });
 
-      // overlay.addEventListener("click", function() {
-      //     jobPopupForm.style.display = "none";
-      //     overlay.style.display = "none";
-      // });
+  // overlay.addEventListener("click", function() {
+  //     jobPopupForm.style.display = "none";
+  //     overlay.style.display = "none";
+  // });
 
+  //   //adding an event listener to the form submission
+  const jobForm = document.getElementById("jobForm");
+  jobForm.addEventListener("submit", handleForm);
 
-    //   //adding an event listener to the form submission
-    const jobForm =document.getElementById("jobForm")
-    jobForm.addEventListener("submit",handleForm)
+  //function to handle the form submission
+  function handleForm(e) {
+    e.preventDefault();
 
-    //function to handle the form submission
-    function handleForm(e){
-      e.preventDefault();
-
-      //retrieve the job details from the form
-      const positionInput = document.getElementById("positionInput").value;
-      const languagesInput = document.getElementById("languagesInput").value;
-      const locationInput = document.getElementById("locationInput").value;
-      const contract = document.getElementById("availabilityInput").value;
-       const link =document.getElementById("linkInput").value
-      //object with the retrieved details
-      const jobsObj={
+    //retrieve the job details from the form
+    const positionInput = document.getElementById("positionInput").value;
+    const languagesInput = document.getElementById("languagesInput").value;
+    const locationInput = document.getElementById("locationInput").value;
+    const contract = document.getElementById("availabilityInput").value;
+    const link = document.getElementById("linkInput").value;
+    //object with the retrieved details
+    const jobsObj = {
       company: "Photosnap",
       logo: "./images/photosnap.svg",
       new: true,
       featured: true,
-      position:positionInput,
+      position: positionInput,
       role: "Frontend",
       level: "Senior",
       postedAt: "Now",
       contract: contract,
-      location:locationInput,
-      languages:[languagesInput],
-      link:link,
-      tools:[]
-      };
-      postJob(jobsObj)
-      document.getElementById('jobForm').reset();
+      location: locationInput,
+      languages: [languagesInput],
+      link: link,
+      tools: [],
+    };
+    postJob(jobsObj);
+    document.getElementById("jobForm").reset();
+  }
 
-      
-    }
-
-    //function to send a POST request 
-      function postJob(jobsObj)
-      {
-      fetch("https://hireme-9kv4.onrender.com/jobs",{
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json"
-        },
-       body:JSON.stringify(jobsObj)
+  //function to send a POST request
+  function postJob(jobsObj) {
+    fetch("https://hireme-9kv4.onrender.com/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jobsObj),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
       })
-      .then(resp =>resp.json())
-      .then(data =>{
-        console.log(data)
-      })
-      .catch(error=>{
-        console.log("error posting data")
-      })
-      }
-      
-      //function to delete a job posting
-      // function deleteJobDetails(id){
-      //   fetch(`https://hireme-9kv4.onrender.com/jobs/${id}`,{
-      //     method:"DELETE",
-      //     headers:{
-      //       "Content-Type":"application/json"
-      //     }
-      //   })
-      //   .then(resp => resp.json())
-      //   .then(data => console.log(data))
-      // }
+      .catch((error) => {
+        console.log("error posting data");
+      });
+  }
 
+  //function to delete a job posting
+  // function deleteJobDetails(id){
+  //   fetch(`https://hireme-9kv4.onrender.com/jobs/${id}`,{
+  //     method:"DELETE",
+  //     headers:{
+  //       "Content-Type":"application/json"
+  //     }
+  //   })
+  //   .then(resp => resp.json())
+  //   .then(data => console.log(data))
+  // }
+};
 
-    }
-
-
-
-
-
-
-
-
-document.addEventListener("DOMContentLoaded" ,initialize)
-
+document.addEventListener("DOMContentLoaded", initialize);
